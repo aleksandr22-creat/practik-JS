3e8164c4f999bba848b4eae1b98a7ae660c6f26c
gr441_hleev <aleksandrharahordin36@gmail.com>
2026-02-16 11:56:18 +0700

    completed 1 and 2 practical tasks

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100
+    }
+
+
+})
+
